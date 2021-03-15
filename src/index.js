d54f5fd7caa20f86101e9e82919f8e182619c343
Rash <gizarash@gmail.com>
2021-03-15 21:56:23 +0600

    feat: implement check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,51 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let strArray = str.split('');
+  let openBrackets = [];
+  let closeBrackets = [];
+  let identicalBrackets = [];
+  
+  bracketsConfig.forEach(element => {
+    if(element[0] == element[1]) {
+      identicalBrackets.push(element[0]);
+    } else {
+      openBrackets.push(element[0]);
+      closeBrackets.push(element[1]);
+    }
+  });
+
+  for (let i = 0; i < strArray.length; i++) {
+    const curBracket = strArray[i];
+    if (openBrackets.indexOf(curBracket) >= 0) {
+      stack.push(curBracket);
+    } else {
+      let closeIndex = closeBrackets.indexOf(curBracket);
+      if (closeIndex >= 0) {
+        if (stack.length > 0) {
+          if (stack[stack.length-1] == openBrackets[closeIndex]) {
+            stack.pop();
+          } else {
+            return false;
+          }
+        } else {
+          return false;
+        }
+      } else {
+        let identicalIndex = identicalBrackets.indexOf(curBracket);
+        if (identicalIndex >= 0) {
+          if (stack.length > 0) {
+            if (stack[stack.length-1] == identicalBrackets[identicalIndex]) {
+              stack.pop();
+            } else {
+              stack.push(curBracket);
+            }
+          } else {
+            stack.push(curBracket);
+          }
+        }
+      }
+    }
+  }
+
+  return (stack.length == 0);
 }
